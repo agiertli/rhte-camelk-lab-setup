7f482dccb2b5fea085ee2ca3b3dff1a6be0b41ca
GPTE Student user9 <agiertli@redhat.com>
2022-11-22 15:05:21 +0100

    more stuff

--- install-argo/install.sh
+++ install-argo/install.sh
@@ -22,19 +22,9 @@ while getopts "p:" opt; do
   esac
 done
 
-# echo "Creating namespace : argocd"
-# oc apply -f namespace.yaml
-# oc project ${ARGOCD_NAMESPACE}
-# echo "Creating OperatorGroup: argocd-operator"
-# oc apply -f operator-group.yaml
+
 echo "Creating Subscription: argocd"
 oc apply -f argocd-subs.yaml
-# echo "Creating ArgoCD CR : ${ARGOCD_CR_NAME}"
-# while [[ argoCdOutput=$(oc apply -f argocd-cr.yaml 2>&1 > /dev/null) == *"error"* ]]
-# do
-#  echo "Waiting until ArgoCD operator is ready..."
-#  sleep 5
-# done
 
 while [[ argoSecret=$(oc -n ${ARGOCD_NAMESPACE} get secret ${ARGOCD_CR_NAME}-cluster 2>&1 > /dev/null) == *"Error"* ]]
 do
@@ -43,19 +33,19 @@ sleep 5
 done
 oc -n $ARGOCD_NAMESPACE patch secret $ARGOCD_CR_NAME-cluster --patch "{\"stringData\": {\"admin.password\": \"$ARGOCD_ADMIN_PASSWORD\"}}"
 
-echo "Logging into the ArgoCD instance..waiting for 30s to allow ArgoCD redeploy after patching"
-sleep 30
-argocd login \
-  --grpc-web \
-  --username admin \
-  --password $ARGOCD_ADMIN_PASSWORD \
-  $(oc get route ${ARGOCD_CR_NAME}-server -n ${ARGOCD_NAMESPACE} --template='{{ .spec.host }}')
+# echo "Logging into the ArgoCD instance..waiting for 30s to allow ArgoCD redeploy after patching"
+# sleep 30
+# argocd login \
+#   --grpc-web \
+#   --username admin \
+#   --password $ARGOCD_ADMIN_PASSWORD \
+#   $(oc get route ${ARGOCD_CR_NAME}-server -n ${ARGOCD_NAMESPACE} --template='{{ .spec.host }}')
 
-echo "Adding Github Repository ${ARGOCD_GIT_URL}"
-argocd repo add ${ARGOCD_GIT_URL} --name ${ARGOCD_GIT_NAME}
+# echo "Adding Github Repository ${ARGOCD_GIT_URL}"
+# argocd repo add ${ARGOCD_GIT_URL} --name ${ARGOCD_GIT_NAME}
 
 
-# echo "Setting ArgoCD permissions"
+echo "Setting ArgoCD permissions"
 oc new-project tooling
 oc policy add-role-to-user \
    edit \
@@ -65,21 +55,11 @@ oc policy add-role-to-user \
 
 echo "Creating ArgoCD Project ${ARGOCD_PROJECT_NAME}"
 
-argocd proj create ${ARGOCD_PROJECT_NAME} \
-   --dest https://kubernetes.default.svc,* \
-   --src ${ARGOCD_GIT_URL} \
-   --description "ArgoCD Project for Tooling"
+oc apply -f tooling-project.yaml -n  ${ARGOCD_NAMESPACE}
 
 echo "Creating ArgoCD App of Apps ${ARGOCD_APP_NAME}"
 
-argocd app create ${ARGOCD_APP_NAME} \
-  --project ${ARGOCD_PROJECT_NAME} \
-  --repo ${ARGOCD_GIT_URL} \
-  --path tooling \
-  --revision HEAD \
-  --dest-namespace ${ARGOCD_NAMESPACE} \
-  --dest-server https://kubernetes.default.svc
-
+oc apply -f app-of-apps.yaml -n ${ARGOCD_NAMESPACE}
 
 argocd app set ${ARGOCD_APP_NAME} --sync-policy automated --self-heal
 #argocd app set ${ARGOCD_APP_NAME} --sync-option CreateNamespace=true

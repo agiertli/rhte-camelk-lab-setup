10dbe079ca994e4294f01b84a9af949a2fc22de1
Anton Giertli <agiertli@redhat.com>
2022-11-24 15:33:39 +0100

    uninstall script still sucks

--- install-argo/uninstall.sh
+++ install-argo/uninstall.sh
@@ -4,6 +4,7 @@ ARGOCD_TOOLING_NAMESPACE=tooling
 oc delete application camelk -n ${ARGOCD_CR_NAMESPACE}
 oc delete appproject rhte-camelk-tooling -n ${ARGOCD_CR_NAMESPACE}
 oc delete appproject default -n ${ARGOCD_CR_NAMESPACE}
+oc delete gitopsservice cluster -n ${ARGOCD_CR_NAMESPACE}
 oc delete argocd openshift-gitops -n ${ARGOCD_CR_NAMESPACE}
 oc delete csv openshift-gitops-operator.v1.6.2 -n ${ARGOCD_OPERATOR_NAMESPACE}
 oc delete subs openshift-gitops-operator -n ${ARGOCD_OPERATOR_NAMESPACE}

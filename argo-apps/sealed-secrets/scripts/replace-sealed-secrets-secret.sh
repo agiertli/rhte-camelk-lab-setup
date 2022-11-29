01d8fd7cbb29d1df45a374a991a4e4b8ab135b63
agiertli <agiertli@redhat.com>
2022-11-29 09:55:32 +0100

    Update replace-sealed-secrets-secret.sh

--- argo-apps/sealed-secrets/scripts/replace-sealed-secrets-secret.sh
+++ argo-apps/sealed-secrets/scripts/replace-sealed-secrets-secret.sh
@@ -2,11 +2,11 @@
 
 echo "WARNING: Deleting existing secrets in 8 seconds..."
 echo
-oc get secret -n sealed-secrets -l sealedsecrets.bitnami.com/sealed-secrets-key
+oc get secret -n tooling -l sealedsecrets.bitnami.com/sealed-secrets-key
 sleep 8
 
-oc delete secret -n sealed-secrets -l sealedsecrets.bitnami.com/sealed-secrets-key
+oc delete secret -n tooling -l sealedsecrets.bitnami.com/sealed-secrets-key
 echo "Creating secret from local drive."
-oc create -f ~/.bitnami/sealed-secrets-secret.yaml -n sealed-secrets
+oc create -f ~/.bitnami/sealed-secrets-secret.yaml -n tooling
 echo "Restarting Sealed Secrets controller."
-oc delete pod -l name=sealed-secrets-controller -n sealed-secrets
+oc delete pod -l name=sealed-secrets-controller -n tooling

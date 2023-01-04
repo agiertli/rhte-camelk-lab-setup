d67c72ab81edf05e2b331570c5ae127757873255
Maz Arslan <marslan@redhat.com>
2023-01-04 13:13:48 +0000

    try

---
+++ install-lab/demo.sh
@@ -0,0 +1,21 @@
+oc delete appproject rhte-camelk-tooling -n openshift-gitops
+oc delete app rhte-camelk-tooling-apps-of-apps -n openshift-gitops
+
+for i in {1..10}
+do
+    oc delete app lab5-user$i-charts-prod -n openshift-gitops
+    oc delete app lab5-user$i-charts-dev -n openshift-gitops
+    oc delete app lab5-user$i-secrets-prod -n openshift-gitops
+    oc delete app lab5-user$i-secrets-dev -n openshift-gitops
+done
+
+oc delete app lab5 -n openshift-gitops
+
+oc delete app amq7 -n openshift-gitops
+oc delete app devspaces-install -n openshift-gitops
+oc delete app devspaces-seed -n openshift-gitops
+oc delete app gitea -n openshift-gitops
+oc delete app namespaces -n openshift-gitops
+oc delete app sealed-secrets -n openshift-gitops
+oc delete app tekton -n openshift-gitops
+oc delete app camelk-community -n openshift-gitops
--- install-lab/uninstall.sh
+++ install-lab/uninstall.sh
@@ -1,12 +1,28 @@
 ARGOCD_CR_NAMESPACE=openshift-gitops
 ARGOCD_OPERATOR_NAMESPACE=openshift-operators
 ARGOCD_TOOLING_NAMESPACE=tooling
-oc delete application camelk -n ${ARGOCD_CR_NAMESPACE}
+# this doent work :?
+# oc delete application camelk -n ${ARGOCD_CR_NAMESPACE}
+
+oc delete app lab5 -n openshift
+
+
 oc delete appproject rhte-camelk-tooling -n ${ARGOCD_CR_NAMESPACE}
 oc delete appproject default -n ${ARGOCD_CR_NAMESPACE}
-oc delete gitopsservice cluster -n ${ARGOCD_CR_NAMESPACE}
-oc delete argocd openshift-gitops -n ${ARGOCD_CR_NAMESPACE}
-oc delete csv openshift-gitops-operator.v1.6.3 -n ${ARGOCD_OPERATOR_NAMESPACE}
-oc delete subs openshift-gitops-operator -n ${ARGOCD_OPERATOR_NAMESPACE}
-oc delete project ${ARGOCD_CR_NAMESPACE}
-oc delete project ${ARGOCD_TOOLING_NAMESPACE}
+# oc delete gitopsservice cluster -n ${ARGOCD_CR_NAMESPACE}
+# oc delete argocd openshift-gitops -n ${ARGOCD_CR_NAMESPACE}
+# oc delete csv openshift-gitops-operator.v1.6.3 -n ${ARGOCD_OPERATOR_NAMESPACE}
+# oc delete subs openshift-gitops-operator -n ${ARGOCD_OPERATOR_NAMESPACE}
+dsc server:delete --delete-all --yes
+# oc delete project ${ARGOCD_CR_NAMESPACE}
+# oc delete project ${ARGOCD_TOOLING_NAMESPACE}
+for i in {1..10}
+do
+    oc delete project user$i-dev
+    oc delete project user$i-prod
+done
+# delete camelk-instructor
+
+# Tekton Delete
+
+# CRD's
